ee3f9581f2f3ebd7af42029a2a6bd812c284552a
Alaa Driedi <51034664+AlaaDuridi@users.noreply.github.com>
2023-12-05 18:41:23 +0200

    Update and rename Owners.php to Owner.php

---
+++ app/Models/Owner.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Owner extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'phone'];
+
+    public function gasStations(): HasMany
+    {
+        return $this->hasMany(GasStation::class, 'owner_id');
+    }
+
+    public function workshops(): HasMany
+    {
+        return $this->hasMany(Workshop::class, 'owner_id');
+    }
+}
+
+/**
+ * ddl
+ * Table Owners {
+  id int [primary key]
+  name varchar(255)
+  phone varchar(20)
+}
+ */
