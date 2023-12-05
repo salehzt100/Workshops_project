4ec30b8ffc965dbeb6191513dfcfbdad2748d8a2
Alaa Driedi <51034664+AlaaDuridi@users.noreply.github.com>
2023-12-05 18:45:54 +0200

    Update WorkshopFinancialProcess.php

--- app/Models/WorkshopFinancialProcess.php
+++ app/Models/WorkshopFinancialProcess.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class WorkshopFinancialProcess extends Model
 {
     use HasFactory;
+    protected $fillable = ['workshop_id',  'count_of_hours_or_cups', 'rate_per_hour_or_cup', 'total_amount'];
+
+    public function workshop()
+    {
+        return $this->belongsTo(Workshop::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class, 'workshop_financial_process_id');
+    }
+    /**
+     * Table WorkshopFinancialProcess {
+  id int [primary key]
+  workshop_id int
+ count_of_hours_or_cups decimal(5,2)
+  rate_per_hour_or_cup decimal(10,2)
+  total_amount decimal(10,2)
+
+}
+     */
 }
