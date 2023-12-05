809e5b6f15e61307cf1a192e1984c80214cc6722
Alaa Driedi <51034664+AlaaDuridi@users.noreply.github.com>
2023-12-05 18:41:02 +0200

    Update GasStationRefill.php

--- app/Models/GasStationRefill.php
+++ app/Models/GasStationRefill.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class GasStationRefill extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    protected $fillable = ['gas_station_id', 'transaction_date', 'total_amount', 'notes'];
+    public function gasStation(): BelongsTo
+    {
+        return $this->belongsTo(GasStation::class, 'gas_station_id');
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class, 'gas_station_refill_id');
+    }
 }
+
+/**
+ * ddl
+ * Table GasStationRefill {
+  id int [primary key]
+  gas_station_id int
+  transaction_date date
+  total_amount decimal(10, 2)
+  notes varchar(255)
+}
+ */
