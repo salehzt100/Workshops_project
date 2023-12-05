e415d5b17d6baeaf53de81463acd26f776b767bc
Alaa Driedi <51034664+AlaaDuridi@users.noreply.github.com>
2023-12-05 18:40:02 +0200

    Update EmployeeOvertime.php

--- app/Models/EmployeeOvertime.php
+++ app/Models/EmployeeOvertime.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmployeeOvertime extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    protected $fillable = ['employee_id', 'date', 'hours_worked', 'rate_per_hour', 'amount'];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'employee_id');
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class, 'employee_overtime_id', 'id');
+    }
 }
