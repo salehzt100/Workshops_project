f71d76474040968612f26145db8b6493a3c6afb6
Alaa Driedi <51034664+AlaaDuridi@users.noreply.github.com>
2023-12-05 18:41:52 +0200

    Update and rename Payments.php to Payment.php

---
+++ app/Models/Payment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+
+class Payment extends Model
+{
+    use HasFactory;
+    protected $guarder = ['id'];
+    protected $fillable = ['payment_type', 'amount_type', 'check_id', 'date', 'employee_overtime_id', 'employee_id', 'gas_station_refill_id', 'expense_id', 'vehicle_income_id', 'workshop_financial_process_id', 'note'];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'employee_id');
+    }
+
+    public function employeeOvertime(): BelongsTo
+    {
+        return $this->belongsTo(EmployeeOvertime::class, 'employee_overtime_id');
+    }
+
+    public function expense(): BelongsTo
+    {
+        return $this->belongsTo(Expense::class);
+    }
+
+    public function vehicleIncome(): BelongsTo
+    {
+        return $this->belongsTo(VehicleIncome::class);
+    }
+
+    public function workshopFinancialProcess(): BelongsTo
+    {
+        return $this->belongsTo(WorkshopFinancialProcess::class);
+    }
+
+    public function check(): HasOne
+    {
+        return $this->hasOne(Check::class);
+    }
+}
+
+/**
+ *
+ *
+Table Payments {
+  id int [primary key]
+  payment_type enum (employee_overtime, employee_advance, employee_salary, station_refill, expenses, vehicle_income, workshop_financial_process  )
+  amount_type enum(cash, check)
+  check_id int
+  date date
+  employee_overtime_id int
+  employee_id int
+  gas_station_refill_id int
+  expense_id int
+  vehicle_income_id int
+  workshop_financial_process_id int
+  note varchar(255)
+}
+ */
