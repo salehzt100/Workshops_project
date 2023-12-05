069a459a7142381aee3c22fae687f12e893578fb
Alaa Driedi <51034664+AlaaDuridi@users.noreply.github.com>
2023-12-05 18:39:33 +0200

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employee extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    protected $fillable = ['name', 'national_id', 'phone', 'salary', 'total_advances'];
+
+
+    public function overtime(): HasMany
+    {
+        return $this->hasMany(EmployeeOvertime::class, 'employee_id');
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class, 'employee_id');
+    }
 }
