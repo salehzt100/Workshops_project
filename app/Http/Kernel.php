45378fe9fd14c4141ab7362407e5281b15e9bda1
Alaa Driedi <153751870+Ala2D@users.noreply.github.com>
2024-01-14 18:43:21 +0200

    add basics for authentication, will optimize it  as soon as possible

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
 class Kernel extends HttpKernel
 {
     /**
@@ -39,9 +41,13 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
         ],
     ];
 
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\v1\WorkshopsController;
 use App\Http\Controllers\v1\CheckController;
 use App\Http\Controllers\v1\VehiclesController;
 use App\Http\Controllers\v1\OwnersController;
+use App\Http\Controllers\v1\UserController;
 use App\Http\Controllers\v1\WithDrawController;
 use App\Http\Controllers\v1\WorkshopFinancialProcessController;
 /*
@@ -25,102 +26,94 @@ use App\Http\Controllers\v1\WorkshopFinancialProcessController;
 */
 
 
+
+
 // Employee routes
 
 Route::prefix('/v1')->group(function () {
-    Route::GET('employees', [EmployeeController::class, 'index']);
-    Route::GET('employees/{id}', [EmployeeController::class, 'show']);
-    Route::PUT('employees/{id}', [EmployeeController::class, 'update']);
-    Route::POST('employees', [EmployeeController::class, 'create']);
-    Route::DELETE('employees/{id}', [EmployeeController::class, 'delete']);
-});
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('login', [UserController::class, 'index']);
 
-// EmployeeOvertime routes
+        Route::GET('employees', [EmployeeController::class, 'index']);
+        Route::GET('employees/{id}', [EmployeeController::class, 'show']);
+        Route::PUT('employees/{id}', [EmployeeController::class, 'update']);
+        Route::POST('employees', [EmployeeController::class, 'create']);
+        Route::DELETE('employees/{id}', [EmployeeController::class, 'delete']);
 
-Route::prefix('/v1')->group(function () {
-    Route::PUT('employeeOvertimes/{id}', [EmployeeOvertimeController::class, 'update']);
-    Route::POST('employee/{id}/employeeOvertimes', [EmployeeOvertimeController::class, 'add']);
-    Route::DELETE('employeeOvertimes/{id}', [EmployeeOvertimeController::class, 'delete']);
-});
+        // EmployeeOvertime routes
 
-// Workshops routes
+        Route::PUT('employeeOvertimes/{id}', [EmployeeOvertimeController::class, 'update']);
+        Route::POST('employee/{id}/employeeOvertimes', [EmployeeOvertimeController::class, 'add']);
+        Route::DELETE('employeeOvertimes/{id}', [EmployeeOvertimeController::class, 'delete']);
 
-Route::prefix('/v1')->group(function () {
-
-    Route::GET('workshops', [WorkshopsController::class, 'index']);
-    Route::GET('workshops/{id}', [WorkshopsController::class, 'show']);
-    Route::PUT('workshops/{id}', [WorkshopsController::class, 'update']);
-    Route::POST('/workshops', [WorkshopsController::class, 'add']);
-    Route::DELETE('workshops/{id}', [WorkshopsController::class, 'delete']);
-    Route::GET('workshops/{id}/payments', [WorkshopsController::class, 'getPayments']);
-    Route::POST('workshops/{id}/payments', [WorkshopsController::class, 'setPayments']);
-    Route::GET('workshops/{id}/vehicles', [WorkshopsController::class, 'getVehicles']);
-    Route::POST('workshops/{workshop_id}/vehicles/{vehicle_id}', [WorkshopsController::class, 'setVehicles']);
-    Route::GET('workshops/{id}/workshopFinancials', [WorkshopsController::class, 'getWorkshopFinancial']);
-    Route::POST('workshops/{id}/workshopFinancials', [WorkshopsController::class, 'setFinancialProcess']);
-});
+        // Workshops routes
 
 
-// Vehicles  routes
-
-Route::prefix('/v1')->group(function () {
-
-    Route::GET('vehicles', [VehiclesController::class, 'index']);
-    Route::GET('vehicles/{id}', [VehiclesController::class, 'show']);
-    Route::PUT('vehicles/{id}', [VehiclesController::class, 'update']);
-    Route::POST('vehicles', [VehiclesController::class, 'add']);
-    Route::DELETE('vehicles/{id}', [VehiclesController::class, 'delete']);
-    Route::GET('vehicles/{id}/payments', [VehiclesController::class, 'getPayments']);
-    Route::POST('vehicles/{id}/payments', [VehiclesController::class, 'setPayments']);
-    Route::GET('vehicles/{id}/expenses', [VehiclesController::class, 'getExpenses']);
-    Route::POST('vehicles/{id}/expenses', [VehiclesController::class, 'setExpenses']);
-});
+        Route::GET('workshops', [WorkshopsController::class, 'index']);
+        Route::GET('workshops/{id}', [WorkshopsController::class, 'show']);
+        Route::PUT('workshops/{id}', [WorkshopsController::class, 'update']);
+        Route::POST('/workshops', [WorkshopsController::class, 'add']);
+        Route::DELETE('workshops/{id}', [WorkshopsController::class, 'delete']);
+        Route::GET('workshops/{id}/payments', [WorkshopsController::class, 'getPayments']);
+        Route::POST('workshops/{id}/payments', [WorkshopsController::class, 'setPayments']);
+        Route::GET('workshops/{id}/vehicles', [WorkshopsController::class, 'getVehicles']);
+        Route::POST('workshops/{workshop_id}/vehicles/{vehicle_id}', [WorkshopsController::class, 'setVehicles']);
+        Route::GET('workshops/{id}/workshopFinancials', [WorkshopsController::class, 'getWorkshopFinancial']);
+        Route::POST('workshops/{id}/workshopFinancials', [WorkshopsController::class, 'setFinancialProcess']);
 
 
-//   check routs
+        // Vehicles  routes
 
-Route::prefix('/v1')->group(function () {
 
-    Route::GET('checks', [CheckController::class, 'index']);
-    Route::GET('checks/{id}', [checkController::class, 'show']);
-    Route::PUT('checks/{id}', [checkController::class, 'update']);
-    Route::POST('checks', [checkController::class, 'add']);
-    Route::DELETE('checks/{id}', [checkController::class, 'delete']);
-});
+        Route::GET('vehicles', [VehiclesController::class, 'index']);
+        Route::GET('vehicles/{id}', [VehiclesController::class, 'show']);
+        Route::PUT('vehicles/{id}', [VehiclesController::class, 'update']);
+        Route::POST('vehicles', [VehiclesController::class, 'add']);
+        Route::DELETE('vehicles/{id}', [VehiclesController::class, 'delete']);
+        Route::GET('vehicles/{id}/payments', [VehiclesController::class, 'getPayments']);
+        Route::POST('vehicles/{id}/payments', [VehiclesController::class, 'setPayments']);
+        Route::GET('vehicles/{id}/expenses', [VehiclesController::class, 'getExpenses']);
+        Route::POST('vehicles/{id}/expenses', [VehiclesController::class, 'setExpenses']);
+
+
+        //   check routs
+
+
+        Route::GET('checks', [CheckController::class, 'index']);
+        Route::GET('checks/{id}', [checkController::class, 'show']);
+        Route::PUT('checks/{id}', [checkController::class, 'update']);
+        Route::POST('checks', [checkController::class, 'add']);
+        Route::DELETE('checks/{id}', [checkController::class, 'delete']);
 
 
 
 
-// Owner routes
+        // Owner routes
 
-Route::prefix('/v1')->group(function () {
-    Route::GET('owners', [OwnersController::class, 'index']);
-    Route::GET('owners/{id}', [OwnersController::class, 'show']);
-    Route::PUT('owners/{id}', [OwnersController::class, 'update']);
-    Route::POST('owners', [OwnersController::class, 'add']);
-    Route::DELETE('owners/{id}', [OwnersController::class, 'delete']);
-});
+        Route::GET('owners', [OwnersController::class, 'index']);
+        Route::GET('owners/{id}', [OwnersController::class, 'show']);
+        Route::PUT('owners/{id}', [OwnersController::class, 'update']);
+        Route::POST('owners', [OwnersController::class, 'add']);
+        Route::DELETE('owners/{id}', [OwnersController::class, 'delete']);
 
 
 
-// workshop financial routes
+        // workshop financial routes
 
-Route::prefix('/v1')->group(function () {
-    Route::GET('workshopFinancials', [WorkshopFinancialProcessController::class, 'index']);
-    Route::GET('workshopFinancials/{id}', [WorkshopFinancialProcessController::class, 'show']);
-    Route::PUT('workshopFinancials/{id}', [WorkshopFinancialProcessController::class, 'update']);
-    Route::DELETE('workshopFinancials/{id}', [WorkshopFinancialProcessController::class, 'delete']);
-});
+        Route::GET('workshopFinancials', [WorkshopFinancialProcessController::class, 'index']);
+        Route::GET('workshopFinancials/{id}', [WorkshopFinancialProcessController::class, 'show']);
+        Route::PUT('workshopFinancials/{id}', [WorkshopFinancialProcessController::class, 'update']);
+        Route::DELETE('workshopFinancials/{id}', [WorkshopFinancialProcessController::class, 'delete']);
 
 
-// gas station routes
+        // gas station routes
 
-Route::prefix('/v1')->group(function () {
-    Route::GET('gasStations', [GasStationController::class, 'index']);
-    Route::GET('gasStations/{id}', [GasStationController::class, 'show']);
-    Route::patch('gasStations/{id}', [GasStationController::class, 'update']);
-    Route::POST('gasStations', [GasStationController::class, 'store']);
-    Route::DELETE('gasStations/{id}', [GasStationController::class, 'destroy']);
-    Route::patch('/gasStations/{id}/add-balance', [GasStationController::class, 'addBalance']);
-    Route::post('/gas-stations/{id}/withdraw-expense', [WithDrawController::class, 'store']);
+        Route::GET('gasStations', [GasStationController::class, 'index']);
+        Route::GET('gasStations/{id}', [GasStationController::class, 'show']);
+        Route::patch('gasStations/{id}', [GasStationController::class, 'update']);
+        Route::POST('gasStations', [GasStationController::class, 'store']);
+        Route::DELETE('gasStations/{id}', [GasStationController::class, 'destroy']);
+        Route::patch('/gasStations/{id}/add-balance', [GasStationController::class, 'addBalance']);
+        Route::post('/gas-stations/{id}/withdraw-expense', [WithDrawController::class, 'store']);
+    });
 });
